e8bcb6629228b479aeb81438c03cd3285410135c
ramon-egger <ramonegger@hotmail.com>
2018-02-22 14:58:29 +0100

    changes..

--- resource/check.sh
+++ resource/check.sh
@@ -1,5 +1,17 @@
 #/bin/bash
 #checking cool stuff
+RED='\033[0;31m'
+NC='\033[0m'
+printf "Hi..\n"
+sleep 1s
+printf "Checking ur shell..\n"
+sleep 1s
 
-clear
-echo ""
+
+
+#
+if [ $0 = "bash" ]; then
+	printf "Your shell is ${RED}$0${NC}\n" && sleep 1s
+else
+	echo "Are you using bash?"
+fi
